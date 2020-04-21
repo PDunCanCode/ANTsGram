bdd70147a1909cdf7615819d2ad147baab74e0fa
Paul D. Duncan <chesiredarling@gmail.com>
2020-04-21 15:34:18 -0700

    the numero uno

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/Login';
+import Header from './components/Header';
+import CreatePost from './components/CreatePost';
+import PostList from './components/PostList';
+
+const functionsCount = new Set();
 
 function App() {
+  const [user, setUser] = React.useState('ANTone');
+  const [posts, setPosts] = React.useState([]);
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    document.title = user ? `${user}'s Feed` : 'Please login';
+  }, [user]);
+
+  const handleAddPost = React.useCallback(
+    (newPost) => {
+      setPosts([newPost, ...posts]);
+    },
+    [posts]
+  );
+
+  functionsCount.add(handleAddPost);
+  console.log(functionsCount);
+
+  if (!user) {
+    return <Login setUser={setUser} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header user={user} setUser={setUser} />
+      <CreatePost user={user} handleAddPost={handleAddPost} />
+      <PostList posts={posts} />
+      <button onClick={() => setCount((prev) => prev + 1)}>{count} +</button>
+    </>
   );
 }
 
